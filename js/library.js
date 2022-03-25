16f9ab6f3a60c2bdac0d1e86c03f75cf0326fe8c
bernardojru <bernardogomes860@gmail.com>
2022-03-25 20:10:43 +0100

    Update All

--- js/library.js
+++ js/library.js
@@ -1,1 +1,7 @@
+const searchBox = document.querySelector("#search-box");
+const nav = document.querySelector(".nav");
+searchBox.addEventListener("click", () => {
+  nav.classList.toggle("nav-show-toggle");
+});
+
 window.confirm("Estamos em mautenção.");
