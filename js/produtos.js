61ad4ae8538c73b1a130d679a425a31efa88540f
bernardojru <bernardogomes860@gmail.com>
2022-04-28 11:18:14 +0100

    new update

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,11 @@
 const searchBox = document.querySelector("#search-box");
 const nav = document.querySelector(".nav");
+const container = document.querySelector(".container-online");
+const btn = document.querySelector(".btn");
 searchBox.addEventListener("click", () => {
   nav.classList.toggle("nav-show-toggle");
+});
+btn.addEventListener("click", () => {
+  container.classList.toggle("container-online");
+  container.classList.toggle("new-container");
 });
